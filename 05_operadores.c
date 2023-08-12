99d0deaa691b7cbc384e849d0733dca6281b8021
Ivan Gonzalez <gohanckz@gmail.com>
2023-08-12 19:29:42 -0400

    Add files via upload

---
+++ 05_operadores.c
@@ -0,0 +1,42 @@
+// Operadores en C
+
+/*
+----- Operadores aritmeticos -----
+OPERADOR    NOMBRE  SIGNIFICA
++           aditivo Suma
+-           inverso aditivo resta
+*           producto multiplicación
+/           Cociente  División
+%           Módulo   Residuo de una división
+++          Incremento  sumar 1
+--          Decremento restar 1
+
+
+----- OPERADORES RELACIONALES -----
+OPERADOR       SIGNIFICADO
+<               Menor que
+>               Mayor que
+<=              Menor igual que
+>=              Mayor o igual que
+==              igual a
+
+
+---- OPERADORES LOGICOS ----
+
+OPERADOR        NOMBRE      SIGNIFICADO
+!               NOT         Negación
+&&              AND         "Y también"
+||              OR          "ó"
+
+
+---- OPERADORES DE ASIGNACIÓN ----
+
+OPERADOR        SIGNIFICA
+=               Asignación
++=              Suma y asignación
+-=              Resta y asignación
+*=              Multiplicación y asignación
+/=              División y asignación
+%=              Módulo y asignación
+
+*/
