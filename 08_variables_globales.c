8286d8ad2ad4864497170442964517c3b3a99eaf
Ivan Gonzalez <gohanckz@gmail.com>
2023-08-14 00:10:43 -0400

    Add files via upload

---
+++ 08_variables_globales.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int variable = 0;
+int main(){
+    printf("El valor de la variable es: %i\n",variable);
+
+    //Le cambiamos el valor e incrementamos
+    for (variable; variable<=20; variable+=2){
+        continue;
+    printf("\n Variable = %i",variable);
+
+    variable = 10 + 5;
+
+    printf("El valor de la variable es = %i",variable);
+    }
+    return 0;
+    
+
+}
