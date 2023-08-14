8668803c29aa5da53cb49d02db52caf743ec7ffb
Ivan Gonzalez <gohanckz@gmail.com>
2023-08-14 19:51:46 -0400

    Add files via upload

---
+++ 12_conversion_anos.c
@@ -0,0 +1,20 @@
+/*
+Cree un programa que convierta una cantidad de años en meses, semanas, días y horas.
+*/
+
+#include <stdio.h>
+
+int main(){
+    int anos,meses,semanas,dias,horas;
+
+    printf("Ingrese la cantidad de anos que desea convertir : ");
+    scanf("%i",&anos);
+
+    meses = anos*12;
+    semanas = meses*4;
+    dias = meses*7;
+    horas = dias*24;
+
+    printf("Los anos ingresados equivalen a %i meses, %i semanas, %i dias y %i horas.",meses,semanas,dias,horas);
+
+}
